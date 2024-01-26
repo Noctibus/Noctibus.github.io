dc09b88d369d0d31ad819f0e6597eb44e64e81f0
Naowak <yannis.bendiouis@gmail.com>
2024-01-26 10:48:10 +0100

    save good dockerfileé

--- website/main.py
+++ website/main.py
@@ -31,4 +31,4 @@ async def process_text(item: Item):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8082)
+    uvicorn.run("main:app", host="0.0.0.0", port=8082)
